221f57f00c5042e2a21053fcbd098283ac10d599
Shannon Ke <shannonke@gatech.edu>
2019-08-29 22:54:47 -0400

    added DFIO

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
 window.onscroll = function() {myFunction()};
 function myFunction() {
     var navbar = document.getElementById("myNavbar");
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
         navbar.className = "w3-bar" + " w3-card-2" + " w3-animate-top" + " w3-white";
     } else {
         navbar.className = navbar.className.replace(" w3-card-2 w3-animate-top w3-white", "");
